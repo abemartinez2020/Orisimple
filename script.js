4a972ef476a66b518e2aa4ec9388afa6f4414219
abemartinez2020 <amm172005@gmail.com>
2020-05-22 19:37:11 -0500

    finished responsive layout

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const menuBurger = document.querySelector('.hero_menu');
+const menu = document.querySelector('.hero_menu');
+const burger = document.querySelector('.menu_burger');
+const elephant = document.querySelector('.hero_elephant');
+const heroList = document.querySelector('.list-disp');
 
-menuBurger.addEventListener('click', () => {
-    menuBurger.classList.toggle('menu_burger--open');
+burger.addEventListener('click', () => {
+    console.log('hello');
+    menu.classList.toggle('menu--open');
+    elephant.classList.toggle('disp-eleph');
+    heroList.classList.toggle('menu--off');
 })
